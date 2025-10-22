a918638e004cc9168da9246d65af762128e465e5
JesthelJhoy <jesthel.jhoy@gmail.com>
2025-10-22 23:36:47 +0200

    Added HTML basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather React App</header>
+      <Weather />
     </div>
   );
 }
