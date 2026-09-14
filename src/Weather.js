9becefb1aba5ceca915a1292c7843ceb6b6b5607
JesthelJhoy <jesthel.jhoy@gmail.com>
2026-09-14 21:24:21 +0200

    search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
+    const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
