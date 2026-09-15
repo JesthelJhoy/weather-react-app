ca3de17d3c956b64c83a6205ad0dc128a2b70b58
JesthelJhoy <jesthel.jhoy@gmail.com>
2026-09-15 22:57:51 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
 
-        <footer>This project was coded by Jesthel.</footer>
+        <footer>
+          This project was coded by Jesthel J. and is{" "}
+          <a
+            href="https://github.com/JesthelJhoy/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://reactweatherapp-jesthel.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify.
+          </a>
+        </footer>
       </div>
     </div>
   );
